13fd8a74f6bbdd87896f729359902b62ee494107
Warren McQuinn <warren.mcquinn@gmail.com>
2014-03-18 04:57:49 -0700

    realigned js to resolve syntax on deploy

--- witlog.js
+++ witlog.js
@@ -1,62 +1,49 @@
 if (Meteor.isClient) {
-  Template.hello.greeting                             = function () {
-    return "Welcome to witlog.";
-  };
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !                            == 'undefined')
-        console.log("You pressed the button");
-    }
-
-
-  });
-    var mic                                           = new Wit.Microphone(document.getElementById("microphone"));
-    var info                                          = function (msg) {
-      document.getElementById("info").innerHTML       = msg;
+    var mic = new Wit.Microphone(document.getElementById("microphone"));
+    var info = function (msg) {
+      document.getElementById("info").innerHTML = msg;
     };
-    mic.onready                                       = function () {
+    mic.onready = function () {
       info("Microphone is ready to record");
     };
-    mic.onaudiostart                                  = function () {
+    mic.onaudiostart = function () {
       info("Recording started");
     };
-    mic.onaudioend                                    = function () {
+    mic.onaudioend = function () {
       info("Recording stopped, processing started");
     };
-    mic.onerror                                       = function (err) {
+    mic.onerror = function (err) {
       info("Error: " + err);
     };
-    mic.onresult                                      = function (intent, entities) {
-      var r                                           = kv("intent", intent);
-        console.log(intent+" "+entities);
+    mic.onresult = function (intent, entities) {
+      var r = kv("intent", intent);
+
       for (var k in entities) {
-        var e                                         = entities[k];
+        var e = entities[k];
 
         if (!(e instanceof Array)) {
-          r +                                         = kv(k, e.value);
+          r += kv(k, e.value);
         } else {
-          for (var i                                  = 0; i < e.length; i++) {
-            r +                                       = kv(k, e[i].value);
+          for (var i = 0; i < e.length; i++) {
+            r += kv(k, e[i].value);
           }
         }
       }
 
-      document.getElementById("result").innerHTML     = r;
+      document.getElementById("result").innerHTML = r;
     };
-    mic.connect("O5FLWRIOBZLPKDOGTAU5GL4KCKIXXUHK");
-
+    mic.connect("INSERT YOUR CLIENT TOKEN");
+    // mic.start();
+    // mic.stop();
 
     function kv (k, v) {
-      if (toString.call(v) !                          == "[object String]") {
-        v                                             = JSON.stringify(v);
+      if (toString.call(v) !== "[object String]") {
+        v = JSON.stringify(v);
       }
-      return k + "                                    =" + v + "\n";
+      return k + "=" + v + "\n";
     }
-
 }
-
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
